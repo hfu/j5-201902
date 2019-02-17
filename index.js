1ef3d0573e622f13949c66087ed6abe9659f7409
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-17 18:23:01 -0500

    update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const five = require('johnny-five')
 const board = new five.Board()
 
 board.on('ready', () => {
-  const led = new five.Led(13)
-  led.blink(1)
+  const led = new five.Led.RGB({
+    pins: { red: 6, green: 5, blue: 3 }
+  })
+  board.repl.inject({led: led})
+  led.on()
+  led.color("#FF0000")
+  led.blink(1000)
 })
